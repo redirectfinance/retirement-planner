03c84207d125f4c13478689f5cf593a04d2cfe4b
Mark Angelo <markangeloestil@gmail.com>
2026-05-21 21:35:14 +0800

    added keywords, opengraphs and twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,38 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Escape the Rat Race",
-  description: "Your personalized financial freedom plan",
+  title: "Escape the Rat Race | Retirement & Financial Freedom Calculator",
+  description: "Plan your path to financial independence with our retirement calculator. Estimate savings, investments, passive income, and early retirement goals.",
+  keywords: [
+    "retirement calculator",
+    "financial freedom",
+    "FIRE calculator",
+    "early retirement",
+    "investment planning",
+    "passive income",
+    "retirement planning",
+  ],  
   verification: 
   {google: "viqESJ_gtGlBqlasY_OVijZc1wYupPZAPlcR7B_WvZg",
   },
+
+  openGraph: {
+    title: "Escape the Rat Race",
+    description:
+      "Interactive retirement and financial independence calculator.",
+    url: "https://escape-the-rat-race.vercel.app",
+    siteName: "Escape the Rat Race",
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Escape the Rat Race",
+    description:
+      "Calculate your journey toward financial independence and retirement.",
+  },  
+
 };
 
 export default function RootLayout({
